11cc28d0bc2137441b974081ab68d0ff06accee2
samitkumarpatel <samitkumarpatel@gmail.com>
2024-09-20 14:01:28 +0000

    more group implementation

--- src/main/java/net/samitkumar/messenger/controller/ApplicationController.java
+++ src/main/java/net/samitkumar/messenger/controller/ApplicationController.java
@@ -3,8 +3,10 @@ package net.samitkumar.messenger.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import net.samitkumar.messenger.entity.Group;
+import net.samitkumar.messenger.entity.GroupMember;
 import net.samitkumar.messenger.entity.User;
 import net.samitkumar.messenger.repository.GroupRepository;
+import net.samitkumar.messenger.repository.MessageRepository;
 import net.samitkumar.messenger.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.core.Authentication;
@@ -19,8 +21,10 @@ import org.springframework.web.servlet.function.ServerRequest;
 import org.springframework.web.servlet.function.ServerResponse;
 
 import java.util.Objects;
+import java.util.Set;
 import java.util.stream.Collectors;
 
+import static java.util.Objects.nonNull;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.web.servlet.function.RequestPredicates.accept;
 import static org.springframework.web.servlet.function.RequestPredicates.contentType;
@@ -54,6 +58,8 @@ public class ApplicationController {
                         .GET("/{groupId}", accept(APPLICATION_JSON), request -> ServerResponse.noContent().build())
                         .PUT("/{groupId}", contentType(APPLICATION_JSON), request -> ServerResponse.noContent().build())
                         .DELETE("/{groupId}", accept(APPLICATION_JSON), request -> ServerResponse.noContent().build())
+                        .GET("/{groupId}/messages", accept(APPLICATION_JSON), messageHandler::groupMessages)
+
                 )
                 .path("/message", builder -> builder
                         .GET("", accept(APPLICATION_JSON), request -> ServerResponse.noContent().build())
@@ -61,6 +67,7 @@ public class ApplicationController {
                         .GET("/{messageId}", accept(APPLICATION_JSON), request -> ServerResponse.noContent().build())
                         .PUT("/{messageId}", contentType(APPLICATION_JSON), request -> ServerResponse.noContent().build())
                         .DELETE("/{messageId}", accept(APPLICATION_JSON), request -> ServerResponse.noContent().build())
+                        .GET("/between/me/and/{targetUserId}", accept(APPLICATION_JSON), messageHandler::messagesBetweenMeAndTargetUser)
                 )
                 .build();
     }
@@ -100,7 +107,6 @@ class UserHandler {
 @RequiredArgsConstructor
 class GroupHandler {
     final GroupRepository groupRepository;
-
     @SneakyThrows
     public ServerResponse newGroup(ServerRequest request) {
         var newGroup = request.body(Group.class);
@@ -108,7 +114,12 @@ class GroupHandler {
         newGroup.setCreatedBy(user.getUserId());
 
         //TODO try to maintain the createdUser in GroupMember Table
-        //newGroup.getMembers().add(GroupMember.builder().userId(user.getUserId()).build());
+        if(nonNull(newGroup.getMembers()) && !newGroup.getMembers().isEmpty()) {
+            newGroup.getMembers().add(GroupMember.builder().userId(user.getUserId()).build());
+        } else {
+            newGroup.setMembers(Set.of(GroupMember.builder().userId(user.getUserId()).build()));
+        }
+
         return ServerResponse.ok().body(groupRepository.save(newGroup));
     }
 
@@ -120,7 +131,8 @@ class GroupHandler {
                 .findAll()
                 .stream()
                 .map(group -> {
-                    if(group.getMembers().stream().anyMatch(groupMember -> groupMember.getUserId().equals(user.getUserId()))) {
+                    if(!group.getCreatedBy().equals(user.getUserId()) &&
+                            group.getMembers().stream().anyMatch(groupMember -> groupMember.getUserId().equals(user.getUserId()))) {
                         return group;
                     }
                     return null;
@@ -138,10 +150,47 @@ class GroupHandler {
         return authentication != null ? (User) authentication.getPrincipal() : User.builder().build();
     }
 
+    @SneakyThrows
     public ServerResponse patchGroup(ServerRequest request) {
-        return ServerResponse.noContent().build();
+        var groupId = Long.parseLong(request.pathVariable("groupId"));
+        var dataToBeUpdated = request.body(Group.class);
+        var groupInDb = groupRepository.findById(groupId).orElseThrow();
+
+        if(nonNull(dataToBeUpdated.getGroupName())) {
+            groupInDb.setGroupName(dataToBeUpdated.getGroupName());
+        }
+
+        if(nonNull(dataToBeUpdated.getMembers())) {
+            groupInDb.getMembers().addAll(dataToBeUpdated.getMembers());
+        }
+        return ServerResponse.ok().body(groupRepository.save(groupInDb));
     }
 }
 
 @Component
-class MessageHandler {}
+class MessageHandler {
+
+    private final MessageRepository messageRepository;
+
+    public MessageHandler(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+    private User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication != null ? (User) authentication.getPrincipal() : User.builder().build();
+    }
+
+    public ServerResponse messagesBetweenMeAndTargetUser(ServerRequest request) {
+        var targetUserId = Long.parseLong(request.pathVariable("targetUserId"));
+        var user = getCurrentUser();
+        return ServerResponse.ok().body(messageRepository.findMessagesBetweenUsers(user.getUserId(), targetUserId));
+    }
+
+    public ServerResponse groupMessages(ServerRequest request) {
+        var groupId = Long.parseLong(request.pathVariable("groupId"));
+        return ServerResponse
+                .ok()
+                .body(messageRepository.findMessagesInGroup(groupId));
+    }
+}
