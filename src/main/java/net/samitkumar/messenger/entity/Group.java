d32b78a73e6e68429def3f784b84b3ac4b7b4139
samitkumarpatel <samitkumarpatel@gmail.com>
2024-11-22 23:48:07 +0100

    WIP for group message Integration

--- src/main/java/net/samitkumar/messenger/entity/Group.java
+++ src/main/java/net/samitkumar/messenger/entity/Group.java
@@ -1,12 +1,15 @@
 package net.samitkumar.messenger.entity;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.NumberSerializers;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.ReadOnlyProperty;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.relational.core.mapping.MappedCollection;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -21,9 +24,10 @@ import java.util.Set;
 @NoArgsConstructor
 public class Group {
     @Id
+    @JsonAlias({"groupId", "userId"})
+    @JsonSerialize
     private Long groupId;
 
-    @JsonAlias({"groupName", "username"})
     private String groupName;
 
     private Long createdBy;
